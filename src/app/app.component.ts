8c083f8ffd37df4adcb11e4c620bdb3e5211f272
ibrahimwickama <ibrahimwickama@gmail.com>
2019-05-21 22:11:30 +0300

    feat: add customm user-sections settings dummy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { userSections } from 'src/assets/config/userSectionsConfig';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hrhis-user-app';
+  
+  userSections = userSections;
 }
---
+++ src/assets/config/userSectionsConfig.ts
@@ -0,0 +1,5 @@
+export const userSections: any[] = [
+    {name: 'Users', description: 'Create, modify, view and delete Users', addRoute: '', viewRoute: ''},
+    {name: 'User Roles', description: 'Create, modify, view and delete User Roles', addRoute: '', viewRoute: ''},
+    {name: 'User Groups', description: 'Create, modify, view and delete User Groups', addRoute: '', viewRoute: ''}
+];
