60e0d66aab4cc126a6b044a9c36a92546dbeb4a4
ibrahimwickama <ibrahimwickama@gmail.com>
2019-06-21 14:36:05 +0300

    feat: include userRole selectors

---
+++ src/app/store/selectors/user-role.selector.ts
@@ -0,0 +1,13 @@
+import { createSelector } from '@ngrx/store';
+import * as fromMainReducer from '../reducers';
+import * as fromUserRoleStateReducer from '../reducers/user-role.reducer';
+
+export const getUserRoleState = createSelector(
+    fromMainReducer.getRootState,
+    (state: fromMainReducer.AppState) => state.userRole
+);
+
+export const getUserRolesList = createSelector(
+    getUserRoleState,
+    fromUserRoleStateReducer.getUserRolesState
+);
